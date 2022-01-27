45b120dc5f65d8c8a21874b66f6283a1ca04bc86
lvictor215 <luizvpeixoto@gmail.com>
2022-01-27 01:47:35 -0300

    aula nova

--- Aulas/teste.py
+++ Aulas/teste.py
@@ -1,79 +1,27 @@
-frase = 'Cri5sdasa jjjbbfuhSDaatGEdcFGAeTRSDa'
+perguntas = {
+    'Pergunta 1': {
+        'pergunta': 'Quanto é 2+2?',
+        'respostas': {'a': '5', 'b': '8', 'c': '3', 'd': '4',},
+        'resposta_certa': 'd',
+    },
+    'Pegunta 2':{
+        'pergunta': 'Quanto é 3*2?',
+        'respostas': {'a': 6, 'b': 8, 'c': 3, 'd': 4,},
+        'resposta_certa': 'a',
+    },
+}
 
 
-def len2(msg):
-    for a, b in enumerate(msg):
-        a += 1
-    return a
+for a, b in perguntas.items():
+    print(f'Pergunta: {b["pergunta"]}')
 
+    for c, d in b['respostas'].items():
+        print(f'[{c}] {d}')
 
-def upFrase(msg):
-    nova_frase = ''
-    for a in msg:
-        if a == "a":
-            a = 'A'
-        elif a == "b":
-            a = "B"
-        elif a == "c":
-            a = "C"
-        elif a == "d":
-            a = "D"
-        elif a == "e":
-            a = "E"
-        elif a == "f":
-            a = "F"
-        elif a == "g":
-            a = "g"
-        elif a == "h":
-            a = "H"
-        elif a == "i":
-            a = "I"
-        elif a == "j":
-            a = "J"
-        elif a == "k":
-            a = "K"
-        elif a == "l":
-            a = "L"
-        elif a == "m":
-            a = "M"
-        elif a == "n":
-            a = "N"
-        elif a == "o":
-            a = "O"
-        elif a == "p":
-            a = "P"
-        elif a == "q":
-            a = "Q"
-        elif a == "r":
-            a = "R"
-        elif a == "s":
-            a = "S"
-        elif a == "t":
-            a = "T"
-        elif a == "u":
-            a = "U"
-        elif a == "v":
-            a = "V"
-        elif a == "w":
-            a = "W"
-        elif a == "x":
-            a = "X"
-        elif a == "y":
-            a = "Y"
-        elif a == "z":
-            a = "Z"
-            
-        nova_frase += a
-    return nova_frase
-
-def lowFrase(msg):
-    nova_frase = ''
-    for a in msg:
-        nova_frase += a.lower()
-    return nova_frase
-
-print(len2(frase))
-print(len(frase))
-
-print(upFrase(frase))
-print(frase.upper())
+    resposta = input("Digite a resposta: ").lower().strip()[0]
+    if resposta == b['resposta_certa']:
+        print("Parabéns, você acertou")
+    else:
+        print("Que pena, você errou!")
+    print()
+    print()
