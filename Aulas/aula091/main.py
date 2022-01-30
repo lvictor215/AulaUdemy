5dbbf6a10f3bb895aa483886a52c805c63f0c2d2
lvictor215 <luizvpeixoto@gmail.com>
2022-01-30 14:58:50 -0300

    Aula decorators

--- Aulas/aula091/main.py
+++ Aulas/aula091/main.py
@@ -1,10 +1,19 @@
-def master():
-    def slave():
-        print('Oi')
-
-    return slave
+# Mutável: Listas, dicionários
+# Imutável: Tuplas, strings, números, True, False, None
 
 
-var = master()
+def lista_de_clientes(clientes_iteravel, lista=None):
+    if lista is None:
+        lista = []
+    lista.extend(clientes_iteravel)
+    return lista
 
-var
+
+lista_clientes_1 = ['Fabrício']
+clientes1 = lista_de_clientes(['João', 'Maria', 'Eduardo'], lista_clientes_1)
+clientes2 = lista_de_clientes(['Marcos', 'Jonas', 'Zico'])
+clientes3 = lista_de_clientes(['José'])
+
+print(clientes1)
+print(clientes2)
+print(clientes3)
